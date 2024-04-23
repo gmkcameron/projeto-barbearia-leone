770f2045fdf401437d52fd5fad8f675d46a180bb
scaleclock4tuna <ygorjosegomes.yj@gmail.com>
2024-04-23 15:46:08 -0300

    version 1.5
    integração do banco

--- form.php
+++ form.php
@@ -1,5 +1,5 @@
-<?php if(isset($_POST["contactsName"]))
-{
+<?php 
+
 	// Read the form values
 	$success = false;
 	$name = isset( $_POST['contactsName'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['contactsName'] ) : "";
@@ -8,7 +8,30 @@
 	$senderTel = isset( $_POST['contactsTel'] ) ? preg_replace( "/[^\s\S\.\-\_\@a-zA-Z0-9]/", "", $_POST['contactsTel'] ) : "";
 	$message = isset( $_POST['contactsMessage'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['contactsMessage'] ) : "";
 
-	//Headers
+	// configurações de credencias 
+	$server =  'localhost';
+	$usario = 'root';
+	$senha = '';
+	$banco = 'barbearia_formulario';
+
+	// conexao com nosso banco de dados
+	$conn = new mysqli($server, $usario, $senha, $banco);
+	// verificar conexao
+	if($conn->connect_error){
+		die("falha ao se comunicar com o banco de dados:".$sconn->connect_error);
+	}
+	$smtp = $sconn-> prepare("INSERT INTO form_clientes(nome, servico, telefone, data, mensagem, horario) VALUES (?,?,?,?,?,?)")
+	$smtp0->bind_param($name, $service, $senderTel, $date, $message);
+
+	if($smtp->execute()){
+		echo "mensagem enviada com sucesso"
+	}else{
+		echo "erro no envio da mensagem:".$smtp->error;
+	}
+
+	$smtp->close();
+	$conn->close();
+	/*Headers
 	$to = "name@domain.com";
     $subject = 'Contact Us';
 	$headers = "MIME-Version: 1.0\r\n";
@@ -18,10 +41,8 @@
 	$message = "Name: ". $name . "<br>Phone: ". $senderTel . "<br>Service: ". $service . "<br>Date: ". $date . "<br> Message: " . $message . "";
 
 	//Email Send Function
-    $send_email = mail($to, $subject, $message, $headers);
-}
-else
-{
-	echo '<div class="failed">Failed: Email not Sent.</div>';
-}
+    $send_email = mail($to, $subject, $message, $headers);*/
+	
+
+
 ?>
